fdf0c71292201addd2b6690497e6ada2451892db
abrarimamsatria09 <092004abrar@gmail.com>
2023-05-10 15:15:06 +0700

    digunakan untuk membuat member akses dan membuat nim 2

--- 042_Pointer/042_Pointer.cpp
+++ 042_Pointer/042_Pointer.cpp
@@ -15,4 +15,8 @@ int main()
 {
 	mahasiswa mhs{ 1 };			//object mhs
 	mhs.shownim();				//member acces operator
+
+	mahasiswa ref = mhs;		//pointer reference refmhs
+	ref.nim = 2;				//member acces operator
+	mhs.shownim();
 }
