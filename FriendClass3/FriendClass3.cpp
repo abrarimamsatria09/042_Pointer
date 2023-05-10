5084a0fa34bc0a72256747f9c5ba85dbcec8bb58
abrarimamsatria09 <092004abrar@gmail.com>
2023-05-10 16:16:05 +0700

    digunakan membuat class pelajar dengan nilai 100

--- FriendClass3/FriendClass3.cpp
+++ FriendClass3/FriendClass3.cpp
@@ -8,3 +8,13 @@ class manusia {
 public:
 	void shownilaipelajar(pelajar&);
 };
+
+class pelajar {
+private:
+	int nilai;
+public:
+	pelajar() {
+		nilai = 100;
+	}
+	friend void manusia::shownilaipelajar(pelajar& x);
+};
